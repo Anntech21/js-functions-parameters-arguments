4cdd8841d98ef701f292b00170340632675efd9d
Laura Hackney <laura@anniecannons.com>
2023-07-30 17:26:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Activity
+
+1. Connect your JavaScript file to your HTML file.
+2. Follow the instructions in the `scripts.js` file and complete the exercises.
---
+++ scripts.js
@@ -0,0 +1,116 @@
+
+/***********************  Exercise 1  ************************/
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+  
+// Call the function multiple times, so you see the following outputs in the console.
+// Output: Hello, Grace!
+// Output: Hello, Jael!
+// Output: Hello, Marcus!
+
+
+/***********************  Exercise 2  ************************/
+function greetMessage(name) {
+    console.log("Hello, " + name + "!");
+}
+  
+// Create a variable called message that is assigned the value of calling the function 'greetingMessage'. Pass in a value to the function.
+
+// What do you see in the console?
+
+  
+
+/***********************  Exercise 3  ************************/
+
+function goToTheCoffeeShop(drink) {
+    alert(`A ${drink} is on the way!`);
+}
+
+// Call this function so that the alert reads "A latte is on the way!"
+
+// Modify the function so that the function takes in two parameters, a drink and a pastry. Call the function with two arguments.
+
+
+/***********************  Exercise 4  ************************/
+// Let’s create a function to calculate the area of a circle.
+// Here is the code for calculating the area of a circle
+let area = Math.PI * radius * radis
+// How many variables will we need?
+// How many parameters?
+// Call the function for a circle with the radius of 15.
+
+
+
+/***********************  Exercise 5  ************************/
+
+function addNumbers[num1, num2] {
+    let result = num1 + num2;
+    console.log(result);
+  }
+
+// Call the above function.
+
+// What errors do you see in the console?
+
+// Fix the funciton so that you can call it without errors.
+
+// Call the above function for the following outputs:
+// Output: 15
+// Output: 34
+// Output: 20
+// Output: 4
+
+
+/***********************  Exercise 6  ************************/
+
+  function showMessage(from, text) { 
+    // Add the code to the code block that will create the following outputs.
+  }
+  
+  showMessage('Ann', 'Hello!'); // Ann: Hello! (*)
+  showMessage('Ann', "What's up?"); // Ann: What's up? (**)
+
+/***********************  Exercise 7  ************************/
+
+// Define a function called multiplyFive which accepts a number and returns that number multiplied by 5.
+
+// Call the function 3 times with different inputs.
+
+
+
+/***********************  Exercise 8  ************************/
+// Create a function that will take in an array as an argument and log the third item in the array to the console. 
+
+
+
+/***********************  Exercise 9  ************************/
+// Create a function that will take in the following object and alert the following, "Welcome, [FULLNAME]. Your email address is [EMAILADDRESS]"
+
+let userObject = {
+    name: {
+        first: "Rose",
+        last: "Smith"
+    },
+    age: 36,
+    programmer: true,
+    favoriteTVShows: ["Fargo", "Breaking Bad", "Battlestar Galactica"],
+    pets: ["Pal"],
+    emailAddress: 'rose@smithcoders.com'
+}
+
+/***********************  Exercise 10  ************************/
+
+function sortArray(numbers) {
+    let max = numbers.sort();
+    console.log(max);
+  }
+  
+const numbersArray = [10, 3, 8, 21, 5];
+const maxNumber = findMaxValue(numbersArray);
+
+
+// Describe what the above code is doing?
+
+// Add code to the above function that will log the smallest number and the largest number to the console in the sentence, "____ is the smallest number, and ____ is the largest number in this dataset. "
+  
